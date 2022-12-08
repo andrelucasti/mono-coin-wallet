bf33523c47073384190560033d74157d873730e2
Andre Lucas <andrelucastic@gmail.com>
2022-12-08 15:28:20 +0000

    SBM: applied recipe 'boot-2.7-3.0-dependency-version-update'

--- wallet-transaction/src/main/java/com/crypto/wallettransaction/dataprovider/purchaseorder/PurchaseOrderTransactionEntity.java
+++ wallet-transaction/src/main/java/com/crypto/wallettransaction/dataprovider/purchaseorder/PurchaseOrderTransactionEntity.java
@@ -8,10 +8,10 @@ import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 import org.hibernate.annotations.TypeDefs;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.time.ZonedDateTime;
 import java.util.Map;
 import java.util.UUID;
